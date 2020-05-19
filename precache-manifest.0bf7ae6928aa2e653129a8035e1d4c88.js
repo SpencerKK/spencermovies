ed75e1c963670b8762436bdc8d0616cd02ad9f27
Spencer Kenealy <kenealys@me.com>
2020-05-19 11:01:13 -0700

    Updates

---
+++ precache-manifest.0bf7ae6928aa2e653129a8035e1d4c88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f7c52121371382192fe31361ea2209",
+    "url": "/spencermovies/index.html"
+  },
+  {
+    "revision": "3430f2b5559f0d2d6135",
+    "url": "/spencermovies/static/css/2.20e4572c.chunk.css"
+  },
+  {
+    "revision": "bf7f78215ea834f7f4ff",
+    "url": "/spencermovies/static/css/main.ec640903.chunk.css"
+  },
+  {
+    "revision": "3430f2b5559f0d2d6135",
+    "url": "/spencermovies/static/js/2.809b02c7.chunk.js"
+  },
+  {
+    "revision": "57b0fc91a9d609bdc0ea69b0817a5930",
+    "url": "/spencermovies/static/js/2.809b02c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7f78215ea834f7f4ff",
+    "url": "/spencermovies/static/js/main.7cd58ebd.chunk.js"
+  },
+  {
+    "revision": "aa6b177bef6fe0af3e14",
+    "url": "/spencermovies/static/js/runtime-main.e5fd46ee.js"
+  },
+  {
+    "revision": "8b10f90b9055f7bbb3bb8d347e6146b8",
+    "url": "/spencermovies/static/media/bg-placeholder.8b10f90b.jpg"
+  },
+  {
+    "revision": "9b2af70f90e8d80ab95fd429498d4854",
+    "url": "/spencermovies/static/media/mdblogo.9b2af70f.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/spencermovies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/spencermovies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/spencermovies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/spencermovies/static/media/slick.f97e3bbf.svg"
+  }
+]);
